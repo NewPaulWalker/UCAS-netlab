4be9508306a8a81e0fc662d2e43fe02f55b12068
linzhida19 <linzhida19@mails.ucas.ac.cn>
2022-03-29 21:28:36 +0800

    lab1 流完成时间实验

--- lab1/fct_exp.py
+++ lab1/fct_exp.py
@@ -8,16 +8,16 @@ class MyTopo(Topo):
     def build(self):
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
-        #bw 带宽 单位Mbps
-        #delay 延时
-        self.addLink(h1, h2, bw=10, delay='10ms')
+        #bw Mbps
+        #delay
+        self.addLink(h1, h2, bw=1000, delay='100ms')
 
 topo = MyTopo()
 net = Mininet(topo = topo, switch = OVSBridge, link = TCLink, controller=None)
 
 net.start()
 h2 = net.get('h2')
-#此处最好用python2.7 而不是python
+#using python2.7 better, rather than python
 h2.cmd('python2.7 -m SimpleHTTPServer 80 &')
 CLI(net)
 h2.cmd('kill %python')
