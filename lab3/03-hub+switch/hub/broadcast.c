f2e2eb248d30c7cdd54da7e9cfaf8da3ba144c36
linzhida19 <linzhida19@mails.ucas.ac.cn>
2022-04-07 22:29:25 +0800

    lab3 实现hub

--- lab3/03-hub+switch/hub/broadcast.c
+++ lab3/03-hub+switch/hub/broadcast.c
@@ -8,4 +8,9 @@ void broadcast_packet(iface_info_t *iface, const char *packet, int len)
 {
 	// TODO: broadcast packet 
 	fprintf(stdout, "TODO: broadcast packet.\n");
+	iface_info_t *entry;
+	list_for_each_entry(entry, &(instance->iface_list), list) {
+		if(entry->fd != iface->fd)
+			iface_send_packet(entry, packet, len);
+	}
 }
